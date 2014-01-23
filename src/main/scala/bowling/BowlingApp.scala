821eba551a021187635ca6d80b82a539cc3f9806
Derek Williams <derek@fyrie.net>
2014-01-23 13:59:48 +0000

    cleanup

--- src/main/scala/bowling/BowlingApp.scala
+++ src/main/scala/bowling/BowlingApp.scala
@@ -1,27 +1,23 @@
 package bowling
 
 object BowlingApp extends App {
-  def process(line: String): Int = {
+  def process(line: Seq[Char]): Int = {
     val lines = line.toList.tails.toList
     val result = ((0, 0, 0, 0) /: lines){
-      case ((total, frameTotal, frame, balls), ball :: tail) =>
-        if (frame < 10) {
-          val ballScore = toScore(ball)
-          ball match {
-            case 'X'    if balls == 0 => (total + 10 + processStrike(tail take 2), 0, frame + 1, 0)
-            case '/'    if balls == 1 => (total + (10 - frameTotal) + processStrike(tail take 1), 0, frame + 1, 0)
-            case ballCh if balls == 0 => (total + ballScore, frameTotal + ballScore,frame, balls + 1)
-            case ballCh if balls == 1 => (total + ballScore, frameTotal + ballScore, frame + 1, 0)
-          }
-        } else {
-          (total, frameTotal, frame, balls)
+      case ((total, frameTotal, frame, balls), ball :: tail) if frame < 10 =>
+        val ballScore = toScore(ball)
+        ball match {
+          case 'X'    if balls == 0 => (total + 10 + processN(tail, 2), 0, frame + 1, 0)
+          case '/'    if balls == 1 => (total + (10 - frameTotal) + processN(tail, 1), 0, frame + 1, 0)
+          case ballCh if balls == 0 => (total + ballScore, frameTotal + ballScore, frame, balls + 1)
+          case ballCh if balls == 1 => (total + ballScore, frameTotal + ballScore, frame + 1, 0)
         }
       case (skipped, _) => skipped
     }
     result._1
   }
 
-  def processStrike(rest: List[Char]): Int = (rest map toScore).sum
+  def processN(rest: Seq[Char], n: Int): Int = (rest take n map toScore).sum
 
   def toScore(ball: Char): Int = ball match {
     case 'X' | '/' => 10
