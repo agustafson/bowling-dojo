16b5bac29f29c57d3842f40c821295188dc9c5b9
James Williams <james@zeebox.com>
2014-01-23 14:07:27 +0000

    Ha. It works

--- src/main/scala/bowling/BowlingApp.scala
+++ src/main/scala/bowling/BowlingApp.scala
@@ -1,5 +1,55 @@
 package bowling
 
 object BowlingApp extends App {
-  println("Hello, bowling-dojo")
+
+  def calculateScore(rolls: String): Int = {
+
+    calculate(frames(insertNils(rolls))) - calculate(frames(insertNils(rolls)).slice(10, 12))
+  }
+
+  def insertNils(rolls: String) : String = {
+     rolls.replaceAll("X", "X-")
+  }
+
+  def frames(rolls: String) : Seq[Frame] = rolls.sliding(2, 2).toList.map { s =>
+    s.toList match {
+      case 'X' :: '-' :: Nil => Frame(10, None)
+      case first :: '-' :: Nil => Frame(Integer.parseInt(first.toString), None)
+      case first :: '/' :: Nil => Frame(Integer.parseInt(first.toString), Some(10 - Integer.parseInt(first.toString)))
+      case first :: second :: Nil => Frame(Integer.parseInt(first.toString), Some(Integer.parseInt(second.toString)))
+      case first :: Nil => Frame(Integer.parseInt(first.toString), None)
+    }
+  }
+
+  private def calculate(frames: Seq[Frame]): Int = {
+    frames match {
+      case Nil => 0
+      case head :: Nil => score(head, Seq())
+      case head :: tail => score(head, tail.take(2)) + calculate(tail)
+    }
+  }
+
+  private def score(frame: Frame, nextFrames: Seq[Frame]): Int = {
+    frame match {
+      case x if x.isStrike => 10 + getScoreForNextTwoRolls(nextFrames)
+      case Frame(x, None) => x
+      case Frame(first, Some(second)) if first + second == 10 => 10 + nextFrames.head.first
+      case Frame(first, Some(second)) => first + second
+    }
+  }
+
+  private def getScoreForNextTwoRolls(nextFrames: Seq[Frame]): Int = {
+    val firstFrame = nextFrames.headOption
+    firstFrame.map { frame =>
+      if (frame.isStrike) {
+        10 + nextFrames.tail.headOption.map(_.first).getOrElse(0)
+      }
+      else frame.first + frame.second.getOrElse(0)
+    }.getOrElse(0)
+  }
 }
+
+case class Frame(first: Int, second: Option[Int]) {
+  def isStrike = first == 10
+}
+
--- src/test/scala/bowling/BowlingSpec.scala
+++ src/test/scala/bowling/BowlingSpec.scala
@@ -4,4 +4,21 @@ import org.specs2.mutable._
 
 class BowlingSpec extends Specification {
 
+  "Bowling score calculator" should {
+    "54545454545454545454 is simple addition" in {
+      BowlingApp.calculateScore("54545454545454545454") === 90
+    }
+    "41414141414141414141 is simpler addition" in {
+      BowlingApp.calculateScore("41414141414141414141") === 50
+    }
+   "9-9-9-9-9-9-9-9-9-9- misses are not counted" in {
+      BowlingApp.calculateScore("9-9-9-9-9-9-9-9-9-9-") === 90
+    }
+    "5/5/5/5/5/5/5/5/5/5/5 is 150" in {
+      BowlingApp.calculateScore("5/5/5/5/5/5/5/5/5/5/5") === 150
+    }
+    "XXXXXXXXXXXX is a winner" in {
+      BowlingApp.calculateScore("XXXXXXXXXXXX") === 300
+    }
+  }
 }
