3283e25136ace722427f953334941074ea45dddc
Derek Williams <derek@fyrie.net>
2014-01-23 13:27:42 +0000

    cleanup

--- src/main/scala/bowling/BowlingApp.scala
+++ src/main/scala/bowling/BowlingApp.scala
@@ -2,17 +2,16 @@ package bowling
 
 object BowlingApp extends App {
   def process(line: String): Int = {
-    val lineAndTails: List[(Char, List[Char])] = line.toList zip (line.toList.tails.toList collect { case Nil => Nil; case x => x.tail })
+    val lineAndTails: List[(Char, List[Char])] = line.toList zip (line.tail.toList.tails.toList)
     val result = ((0, 0, 0, 0) /: lineAndTails){
       case ((total, frameTotal, frame, balls), (ball, tail)) =>
         if (frame < 10) {
+          val ballScore = toScore(ball)
           ball match {
             case 'X' if balls == 0 => (total + 10 + processStrike(tail take 2), 0, frame + 1, 0)
             case '/' if balls == 1 => (total + (10 - frameTotal) + processStrike(tail take 1), 0, frame + 1, 0)
-            case '-' if balls == 0 => (total, frameTotal, frame, balls + 1)
-            case '-' => (total, frameTotal, frame + 1, 0)
-            case ballCh if balls == 0 => (total + ballCh.toString.toInt, frameTotal + ballCh.toString.toInt,frame, balls + 1)
-            case ballCh => (total + ballCh.toString.toInt,frameTotal + ballCh.toString.toInt, frame + 1, 0)
+            case ballCh if balls == 0 => (total + ballScore, frameTotal + ballScore,frame, balls + 1)
+            case ballCh => (total + ballScore, frameTotal + ballScore, frame + 1, 0)
           }
         } else {
           (total, frameTotal, frame, balls)
@@ -24,7 +23,7 @@ object BowlingApp extends App {
   def processStrike(rest: List[Char]): Int = (rest map toScore).sum
 
   def toScore(ball: Char): Int = ball match {
-    case 'X' => 10
+    case 'X' | '/' => 10
     case '-' => 0
     case ch if ch.isDigit => ch.toString.toInt
   }
--- src/test/scala/bowling/BowlingSpec.scala
+++ src/test/scala/bowling/BowlingSpec.scala
@@ -6,7 +6,9 @@ class BowlingSpec extends Specification {
 
   val testCases = ("9-9-9-9-9-9-9-9-9-9-", 90) ::
                   ("XXXXXXXXXXXX", 300) ::
-                  ("5/5/5/5/5/5/5/5/5/5/5", 150) :: Nil
+                  ("5/5/5/5/5/5/5/5/5/5/5", 150) ::
+                  ("23232323232323232323", 50) ::
+                  ("232323232323X232323", 60) :: Nil
 
 
   "processing a line" ! {
