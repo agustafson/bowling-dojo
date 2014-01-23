a0960c6cd06b4786e5fae9a3a87edff83936f3ce
cjwebb <colin@zeebox.com>
2014-01-23 13:57:46 +0000

    failing first attempt

--- src/main/scala/bowling/BowlingApp.scala
+++ src/main/scala/bowling/BowlingApp.scala
@@ -1,5 +1,27 @@
 package bowling
 
 object BowlingApp extends App {
-  println("Hello, bowling-dojo")
+  def calculate(s: String): Int = {
+
+    def recur(l: List[Char], total: Int = 0, previous: Int = 0): Int = {
+      l match {
+        case h :: tail if h == '-' => recur(tail, total, h)
+        case h :: h2 :: tail if h == '/' => recur(tail, (total - previous) + 10 + tail.head, h)
+        case h :: tail if h == 'X' => recur(tail, total + 10, h)
+        case h :: tail => recur(tail, total + h.toString.toInt, h)
+        case Nil => total
+      }
+    }
+
+    recur(s.toCharArray.toList)
+
+  }
+
+  // spare
+  //  look back 1
+  //  subtract value of previous, then add 10, and the bonus
+
+  // strike
+    // count value of next 2
+
 }
--- src/test/scala/bowling/BowlingSpec.scala
+++ src/test/scala/bowling/BowlingSpec.scala
@@ -1,7 +1,22 @@
 package bowling
 
 import org.specs2.mutable._
+import BowlingApp.calculate
 
 class BowlingSpec extends Specification {
+  "stuff" should {
+    "things" in {
+      calculate("--------------------") must be_==(0)
+      calculate("1-1-1-1-1-1-1-1-1-1-") must be_==(10)
+      calculate("2-2-2-2-2-2-2-2-2-2-") must be_==(20)
 
+    }
+    "handle spares" in {
+      calculate("------------------5/-") must be_==(10)
+    }
+
+    "handle strikes" in {
+      calculate("XXXXXXXXXXXX") must be_==(300)
+    }
+  }
 }
