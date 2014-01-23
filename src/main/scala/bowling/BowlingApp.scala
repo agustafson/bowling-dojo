1f78d20268ae94b5ab227e4e69f8752997cbe1d0
Derek Williams <derek@fyrie.net>
2014-01-23 13:08:51 +0000

    test cases pass

--- src/main/scala/bowling/BowlingApp.scala
+++ src/main/scala/bowling/BowlingApp.scala
@@ -1,5 +1,32 @@
 package bowling
 
 object BowlingApp extends App {
-  println("Hello, bowling-dojo")
+  def process(line: String): Int = {
+    val lineAndTails: List[(Char, List[Char])] = line.toList zip (line.toList.tails.toList collect { case Nil => Nil; case x => x.tail })
+    val result = ((0, 0, 0, 0) /: lineAndTails){
+      case ((total, frameTotal, frame, balls), (ball, tail)) =>
+        if (frame < 10) {
+          println((total, frame, balls, ball, tail))
+          ball match {
+            case 'X' if balls == 0 => (total + 10 + processStrike(tail take 2), 0, frame + 1, 0)
+            case '/' if balls == 1 => (total + (10 - frameTotal) + processStrike(tail take 1), 0, frame + 1, 0)
+            case '-' if balls == 0 => (total, frameTotal, frame, balls + 1)
+            case '-' => (total, frameTotal, frame + 1, 0)
+            case ballCh if balls == 0 => (total + ballCh.toString.toInt, frameTotal + ballCh.toString.toInt,frame, balls + 1)
+            case ballCh => (total + ballCh.toString.toInt,frameTotal + ballCh.toString.toInt, frame + 1, 0)
+          }
+        } else {
+          (total, frameTotal, frame, balls)
+        }
+    }
+    result._1
+  }
+
+  def processStrike(rest: List[Char]): Int = (rest map toScore).sum
+
+  def toScore(ball: Char): Int = ball match {
+    case 'X' => 10
+    case '-' => 0
+    case ch if ch.isDigit => ch.toString.toInt
+  }
 }
--- src/test/scala/bowling/BowlingSpec.scala
+++ src/test/scala/bowling/BowlingSpec.scala
@@ -4,4 +4,16 @@ import org.specs2.mutable._
 
 class BowlingSpec extends Specification {
 
+  val testCases = ("9-9-9-9-9-9-9-9-9-9-", 90) ::
+                  ("XXXXXXXXXXXX", 300) ::
+                  ("5/5/5/5/5/5/5/5/5/5/5", 150) :: Nil
+
+
+  "processing a line" ! {
+    forall(testCases) {
+      case (testCase, expected) =>
+        BowlingApp process testCase must_== expected
+    }
+  }
+
 }
