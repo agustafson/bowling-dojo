4e64fdd77e565640541251788969551084690722
Nick Faulkner <nickbfaulkner@gmail.com>
2014-01-23 13:28:32 +0000

    some code, innit

--- src/main/scala/bowling/BowlingApp.scala
+++ src/main/scala/bowling/BowlingApp.scala
@@ -1,5 +1,42 @@
 package bowling
 
 object BowlingApp extends App {
-  println("Hello, bowling-dojo")
+//  def getScore
+
+
 }
+
+
+case class BowlingScore(frames: List[Frame]) {
+  val total = {
+    ((List(Frame(0, Some(0)),
+           Frame(0, Some(0)),
+           Frame(0, Some(0))) ++ frames).sliding(3) map { group =>
+      val thisFrame = group(2)
+      val lastFrame = group(1)
+
+      if (lastFrame.total == 10) {
+        if(lastFrame.firstBall == 10) thisFrame.total * 2
+        else thisFrame.firstBall * 2 + thisFrame.secondBall.getOrElse(0)
+      } else {
+        thisFrame.total
+      }
+    }).toList.sum
+  }
+  def addFrame(frame :Frame): BowlingScore = {
+    BowlingScore(frames :+ frame)
+  }
+}
+
+case class Frame(firstBall: Int, secondBall: Option[Int]){
+
+  require(!(firstBall == 10 && secondBall.isDefined), "Strike in first roll but we've got a value in the second as well")
+  require(firstBall + secondBall.getOrElse(0) <= 10, "Frame sum more than 10!!")
+
+  val total = firstBall + secondBall.getOrElse(0)
+}
+
+object BowlingScore {
+  val start = BowlingScore(List())
+}
+
--- src/test/scala/bowling/BowlingSpec.scala
+++ src/test/scala/bowling/BowlingSpec.scala
@@ -3,5 +3,65 @@ package bowling
 import org.specs2.mutable._
 
 class BowlingSpec extends Specification {
+  "A bowling session" should {
+    "start score of 0" in {
+      val score = BowlingScore.start
 
+      score.frames should beEmpty
+      score.total should_== 0
+    }
+
+
+    "have a score of 5 after one frame if 5 is scored in the first frame" in {
+      val score = BowlingScore.start.addFrame(Frame(5, Some(0)))
+      score.total should_== 5
+    }
+
+    "have a score of 10 after one frame and two rolls of 3 and 7" in {
+      val score = BowlingScore.start.addFrame(Frame(3, Some(7)))
+      score.total should_== 10
+    }
+
+    "have a score of 10 after two frames with scores of 3 and 7" in {
+      val score = BowlingScore.start.addFrame(Frame(3, Some(0)))
+                                    .addFrame(Frame(2, Some(5)))
+      score.total should_== 10
+    }
+
+    "ensure that there was no second ball bowled after a strike" in {
+      Frame(10, Some(1)) should throwAn[IllegalArgumentException]
+    }
+
+    "ensure that the total for a frame doesn't exceed 10" in {
+      Frame(5, Some(6)) should throwAn[IllegalArgumentException]
+    }
+
+    "after a spare the value of the successive frame should be its double plus 10" in {
+      val score = BowlingScore.start.addFrame(Frame(3, Some(7)))
+                                    .addFrame(Frame(4, Some(0)))
+      score.total should_== 18
+    }
+
+    "after a spare the value of the successive frame should be its double (of only the first ball) plus 10" in {
+      val score = BowlingScore.start.addFrame(Frame(3, Some(7)))
+                                    .addFrame(Frame(4, Some(2)))
+      score.total should_== 20
+    }
+
+    "after a strike the value of the successive frame should be double it's total" in {
+      val score = BowlingScore.start.addFrame(Frame(10, None))
+                                    .addFrame(Frame(4, Some(2)))
+
+      score.total should_== 22
+    }
+
+    "after 3 strikes, the total should be 50" in {
+      val score = BowlingScore.start.addFrame(Frame(10, None))
+                                    .addFrame(Frame(10, None))
+                                    .addFrame(Frame(10, None))
+
+      score.total should_==50
+    }
+
+  }
 }
