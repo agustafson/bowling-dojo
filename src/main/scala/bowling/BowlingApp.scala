a029290673345df408f1312e765c47e9a4c42d19
Derek Williams <derek@fyrie.net>
2014-01-23 13:56:23 +0000

    cleanup

--- src/main/scala/bowling/BowlingApp.scala
+++ src/main/scala/bowling/BowlingApp.scala
@@ -2,20 +2,21 @@ package bowling
 
 object BowlingApp extends App {
   def process(line: String): Int = {
-    val lineAndTails: List[(Char, List[Char])] = line.toList zip (line.tail.toList.tails.toList)
-    val result = ((0, 0, 0, 0) /: lineAndTails){
-      case ((total, frameTotal, frame, balls), (ball, tail)) =>
+    val lines = line.toList.tails.toList
+    val result = ((0, 0, 0, 0) /: lines){
+      case ((total, frameTotal, frame, balls), ball :: tail) =>
         if (frame < 10) {
           val ballScore = toScore(ball)
           ball match {
-            case 'X' if balls == 0 => (total + 10 + processStrike(tail take 2), 0, frame + 1, 0)
-            case '/' if balls == 1 => (total + (10 - frameTotal) + processStrike(tail take 1), 0, frame + 1, 0)
+            case 'X'    if balls == 0 => (total + 10 + processStrike(tail take 2), 0, frame + 1, 0)
+            case '/'    if balls == 1 => (total + (10 - frameTotal) + processStrike(tail take 1), 0, frame + 1, 0)
             case ballCh if balls == 0 => (total + ballScore, frameTotal + ballScore,frame, balls + 1)
-            case ballCh => (total + ballScore, frameTotal + ballScore, frame + 1, 0)
+            case ballCh if balls == 1 => (total + ballScore, frameTotal + ballScore, frame + 1, 0)
           }
         } else {
           (total, frameTotal, frame, balls)
         }
+      case (skipped, _) => skipped
     }
     result._1
   }
